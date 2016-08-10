e0b8b4d6ad475a4559391e8270a8eda2460ef2a8
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-08-10 00:58:46 -0500

    Add error method to temp history model and refactor other methods.

--- app/models/temp_history.rb
+++ app/models/temp_history.rb
@@ -1,19 +1,29 @@
 require 'httparty'
 
 class TempHistory
-  def initialize(zip)
+  def initialize(year, month, day, zip)
     @response = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/forecast/history_#{year}#{month}#{day}/q/#{zip}.json")
   end
 
+  def errors?
+    if @response["response"]["error"]
+      return true
+    end
+  end
+
   def high_temp
-    @response["history"]["dailysummary"]["maxtempi"].to_f
+    @response["history"]["dailysummary"][0]["maxtempi"]
   end
 
   def low_temp
-    @response["history"]["dailysummary"]["mintempi"].to_f
+    @response["history"]["dailysummary"][0]["mintempi"]
   end
 
   def mean_temp
-    @response["history"]["dailysummary"]["meantempi"].to_f
+    @response["history"]["dailysummary"][0]["meantempi"]
+  end
+
+  def string_temp
+    @response["history"]["dailysummary"][0]["meantempi"]
   end
 end

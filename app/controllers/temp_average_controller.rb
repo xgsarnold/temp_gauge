e2b17f6e1ea691627f5de1eedbd6cccf5e9c0d77
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-08-10 12:04:44 -0500

    Refactor result view to display zip code and dates selected on previous page.

--- app/controllers/temp_average_controller.rb
+++ app/controllers/temp_average_controller.rb
@@ -6,16 +6,16 @@ class TempAverageController < ApplicationController
     daily_average = []
     high_average = []
     low_average = []
-    zip = params[:zip]
-    begin_date = Date.new(params[:temp_history]["begin_date(1i)"].to_i, params[:temp_history]["begin_date(2i)"].to_i, params[:temp_history]["begin_date(3i)"].to_i)
-    end_date = Date.new(params[:temp_history]["end_date(1i)"].to_i, params[:temp_history]["end_date(2i)"].to_i, params[:temp_history]["end_date(3i)"].to_i)
-    date_range = (begin_date..end_date).to_a
+    @zip = params[:zip]
+    @begin_date = Date.new(params[:temp_history]["begin_date(1i)"].to_i, params[:temp_history]["begin_date(2i)"].to_i, params[:temp_history]["begin_date(3i)"].to_i)
+    @end_date = Date.new(params[:temp_history]["end_date(1i)"].to_i, params[:temp_history]["end_date(2i)"].to_i, params[:temp_history]["end_date(3i)"].to_i)
+    date_range = (@begin_date..@end_date).to_a
     if date_range.first.nil?
       redirect_to temp_average_search_path, notice: 'Please select select earlier date first.'
       return
     end
     date_range.each do |d|
-      temp = TempHistory.new(d.year, ('%02d' % d.month), ('%02d' % d.day), zip)
+      temp = TempHistory.new(d.year, ('%02d' % d.month), ('%02d' % d.day), @zip)
       unless temp.errors?
         daily_average << temp.mean_temp.to_f
         high_average << temp.high_temp.to_f

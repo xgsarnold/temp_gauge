f08675754136c577e8c6c2b3849980b6f08a42de
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-08-10 10:42:16 -0500

    Refactor result action in temp average controller to catch invalid zip codes and improper ordering of dates.

--- app/controllers/temp_average_controller.rb
+++ app/controllers/temp_average_controller.rb
@@ -10,11 +10,20 @@ class TempAverageController < ApplicationController
     begin_date = Date.new(params[:temp_history]["begin_date(1i)"].to_i, params[:temp_history]["begin_date(2i)"].to_i, params[:temp_history]["begin_date(3i)"].to_i)
     end_date = Date.new(params[:temp_history]["end_date(1i)"].to_i, params[:temp_history]["end_date(2i)"].to_i, params[:temp_history]["end_date(3i)"].to_i)
     date_range = (begin_date..end_date).to_a
+    if date_range.first.nil?
+      redirect_to temp_average_search_path, notice: 'Please select select earlier date first.'
+      return
+    end
     date_range.each do |d|
       temp = TempHistory.new(d.year, ('%02d' % d.month), ('%02d' % d.day), zip)
-      daily_average << temp.mean_temp.to_f
-      high_average << temp.high_temp.to_f
-      low_average << temp.low_temp.to_f
+      unless temp.errors?
+        daily_average << temp.mean_temp.to_f
+        high_average << temp.high_temp.to_f
+        low_average << temp.low_temp.to_f
+      else
+        redirect_to temp_average_search_path, notice: 'Please enter a valid zip code.'
+        return
+      end
     end
     @average_temp = daily_average.reduce(:+)/daily_average.length
     @high = high_average.reduce(:+)/high_average.length

087d5287c4434e7a9a6dd5e6f86efdf0bebe0728
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-08-10 10:30:16 -0500

    Add root to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'temp_average/result'
 
+  root 'temp_average#search'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

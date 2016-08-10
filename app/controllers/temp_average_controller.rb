493e6246041df58e65ba15cef6e1789165551150
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-08-10 10:13:58 -0500

    Refactor result action in temp average controller to grab user input and create temp history objects for each date.

--- app/controllers/temp_average_controller.rb
+++ app/controllers/temp_average_controller.rb
@@ -3,5 +3,21 @@ class TempAverageController < ApplicationController
   end
 
   def result
+    daily_average = []
+    high_average = []
+    low_average = []
+    zip = params[:zip]
+    begin_date = Date.new(params[:temp_history]["begin_date(1i)"].to_i, params[:temp_history]["begin_date(2i)"].to_i, params[:temp_history]["begin_date(3i)"].to_i)
+    end_date = Date.new(params[:temp_history]["end_date(1i)"].to_i, params[:temp_history]["end_date(2i)"].to_i, params[:temp_history]["end_date(3i)"].to_i)
+    date_range = (begin_date..end_date).to_a
+    date_range.each do |d|
+      temp = TempHistory.new(d.year, ('%02d' % d.month), ('%02d' % d.day), zip)
+      daily_average << temp.mean_temp.to_f
+      high_average << temp.high_temp.to_f
+      low_average << temp.low_temp.to_f
+    end
+    @average_temp = daily_average.reduce(:+)/daily_average.length
+    @high = high_average.reduce(:+)/high_average.length
+    @low = low_average.reduce(:+)/low_average.length
   end
 end
